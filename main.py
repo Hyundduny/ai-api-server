6c37801c80092b845111a289b9eeb027fb5470eb
Hyundduny <dlguswns0909@gmail.com>
2025-02-20 21:39:49 +0900

    Update

--- main.py
+++ main.py
@@ -9,19 +9,26 @@ app = FastAPI()
 
 operator_model = None
 
-# 모델의 예측 기능을 호출한다. 조회 기능은 GET로 한다.
-@app.post("/model/{type}") 
-def load_model(type: str):
+# 모델을 호출한다.
+@app.post("/model/train/{type}") 
+def train_model(type: str):
     global operator_model
     operator_model = model.Model(type)
     operator_model.train()
     return {"result": "OK"}
 
-# 모델의 예측 기능을 호출한다. 조회 기능은 GET로 한다.
+@app.post("/model/load/{type}") 
+def load_model(type: str):
+    global operator_model
+    operator_model = model.Model(type)
+    operator_model.load_model()
+    return {"result": "OK"}
+
+# 모델의 예측 기능을 호출한다.
 @app.get("/model/test") 
 def test_model():
     if operator_model is None:
         return {"error": "Model not loaded. Please load a model first."}
     
-    output = operator_model.predict()  # NumPy 배열로 반환됨
-    return {"prediction": output.tolist()}  # 리스트 형태로 변환하여 반환
+    output = operator_model.predict()
+    return {"prediction": output.tolist()}
--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pickle
+import os
 
 class Model:
     def __init__(self, model_type):
@@ -17,11 +19,11 @@ class Model:
 
         self.inputs = logic_gates[model_type]["inputs"]
         self.outputs = logic_gates[model_type]["outputs"]
-
+    
+    def train(self):
         self.weights = np.random.rand(self.inputs.shape[1])  # 입력 개수에 맞는 가중치
         self.bias = np.random.rand(1)  # 편향 초기화
-
-    def train(self):
+    
         learning_rate = 0.1
         epochs = 20   
         for epoch in range(epochs):
@@ -42,9 +44,47 @@ class Model:
                 self.bias += learning_rate * error
                 print('====')        
 
+    def load_model(self):
+        file_name = f"model/{self.model_type}.pkl"
+        
+        if not os.path.exists(file_name):
+            print(f"Error: Model file {file_name} not found.")
+            return False
+
+        with open(file_name, "rb") as f:
+            model_data = pickle.load(f)
+        print(model_data)
+        self.weights = model_data["weight"]
+        self.bias = model_data["bias"]
+        print(f"Model {self.model_type} loaded successfulSy from {file_name}")
+        return True
+    
     def step_function(self, x):
         return np.where(x >= 0, 1, 0)  # 벡터 연산 적용
 
     def predict(self):
         total_input = np.dot(self.inputs, self.weights) + self.bias
         return self.step_function(total_input)  # 벡터 연산 적용
+    
+    def save_model(self):
+        model_data = {
+            "weight": self.weights,
+            "bias": self.bias
+        }
+        file_name = f"model/{self.model_type}.pkl"
+        with open(file_name, "wb") as f:
+            pickle.dump(model_data, f)
+        print(f"Model saved to {file_name}")
+
+
+
+
+def main():
+    for model_type in ["AND", "OR", "NOT", "XOR"]:
+        model = Model(model_type)
+        model.train()
+        model.save_model()
+
+
+if __name__ == "__main__":
+    main()
